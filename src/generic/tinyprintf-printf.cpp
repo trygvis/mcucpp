e3b95c6e1ac29e5baf961b4099c63fa9c50ccbec
Trygve Laugstøl <trygvis@inamo.no>
2017-05-13 10:26:23 +0200

    o Preventing double newlines when using printf(), puts() adds a newline, so remove the trailing newline if there.

--- src/generic/tinyprintf-printf.cpp
+++ src/generic/tinyprintf-printf.cpp
@@ -3,18 +3,23 @@
 
 #define TINYPRINTF_DEFINE_TFP_PRINTF 0
 #define TINYPRINTF_OVERRIDE_LIBC 0
+
 #include "tinyprintf.h"
 
 extern "C"
-int printf(const char *format, ...)
-{
+int printf(const char *format, ...) {
     const size_t buffer_size = 1000;
     char buf[buffer_size];
 
     va_list args;
     va_start(args, format);
-    tfp_vsnprintf(buf, buffer_size, format, args);
+    int size = tfp_vsnprintf(buf, buffer_size, format, args);
     va_end(args);
+
+    // Remove the last \n so we can use puts() (which add a \n).
+    if (buf[size - 1] == '\n') {
+        buf[size - 1] = 0;
+    }
 
     puts(buf);
 
