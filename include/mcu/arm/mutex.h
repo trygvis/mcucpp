836a55affa84e749e406b38fb23d0d54eb316b4d
Trygve Laugstøl <trygvis@inamo.no>
2017-06-03 20:48:14 +0200

    o Renaming class-wide locked to locked_.

--- include/mcu/arm/mutex.h
+++ include/mcu/arm/mutex.h
@@ -4,36 +4,36 @@ namespace mcu {
 namespace arm {
 
 class mutex {
-    volatile uint8_t locked;
+    volatile uint8_t locked_;
 
 public:
-    mutex() noexcept : locked(0)
+    mutex() noexcept : locked_(0)
     {}
 
     __always_inline
     bool try_lock()
     {
-        auto l = __LDREXB(&locked);
+        auto l = __LDREXB(&locked_);
 
         if (l == 1) {
             return false;
         }
 
-        return __STREXB(1, &locked) == 0;
+        return __STREXB(1, &locked_) == 0;
     }
 
     __always_inline
     bool lock()
     {
         do {
-            while (__LDREXB(&locked) == 1);
-        } while (__STREXB(1, &locked) != 0);
+            while (__LDREXB(&locked_) == 1);
+        } while (__STREXB(1, &locked_) != 0);
     }
 
     __always_inline
     bool unlock()
     {
-        locked = 0;
+        locked_ = 0;
     }
 };
 
